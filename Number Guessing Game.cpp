3ec44cf7a3b664aa711ee15054b605f98eb71050
22MH1A05F7 <119723363+22MH1A05F7@users.noreply.github.com>
2023-11-16 21:53:58 +0530

    Number Guessing Game.cpp

---
+++ Number Guessing Game.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+	int secretnumber, guess;
+	secretnumber = rand() % 100 + 1;
+	cout << "<<---Welcome to the 🤔  guess 💡 a number game.🤔  Guess a number from 1 to 💯 !-->>\n\n";
+        int attempts=0;
+	do
+	{
+		cout << "Enter a guess between 1️ and 100 :➡️  ";
+		cin >> guess;
+		attempts++;
+
+		if (guess > secretnumber)
+			cout << "<<---Too high! Try Again!--->>\n\n";
+		else if (guess < secretnumber)
+			cout << "<<---Too low! Try Again!--->>\n\n";
+		else
+			cout << "\n<<---🎊 🎊  Congratulation  🎉 🎉! You got correct guesses!--->\n";
+	} while (guess != secretnumber);
+
+	return 0;
+}
